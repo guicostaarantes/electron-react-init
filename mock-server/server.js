b1577af06f2a2e265654f62cb078d6d19ccb44f6
Guilherme Costa Arantes <arantes.g@outlook.com>
2020-04-02 22:21:32 -0300

    added mock server and envs

---
+++ mock-server/server.js
@@ -0,0 +1,5 @@
+const app = require("./app");
+
+app.listen(6464, function() {
+  console.log("Mock server listening to port 6464.");
+});
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { AppState } from "../store/reducers";
@@ -6,6 +6,7 @@ import { useTranslator } from "../utils/translations";
 import EKey from "../utils/translations/EKey";
 import { Button, Grid, Typography } from "@material-ui/core";
 import { useStyles } from "./styles/About";
+import api from "../utils/services/api";
 
 export default function About() {
   const language = useSelector((state: AppState) => state.preferences.language);
@@ -14,14 +15,38 @@ export default function About() {
 
   const classes = useStyles();
 
+  const [apiMessage, setApiMessage] = useState("");
+
+  const fetchApiMessage = async () => {
+    if (apiMessage === "") {
+      const response = await api.get("/");
+      setApiMessage(response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchApiMessage();
+  }, []);
+
   return (
     <div className={classes.root}>
       <Grid container justify="center">
         <Grid container>
-          <Typography className={classes.aboutText}>{translator.get(EKey.about)}</Typography>
+          <Typography className={classes.aboutText}>
+            {translator.get(EKey.about)}
+          </Typography>
         </Grid>
         <Grid container>
-          <Button className={classes.button} variant={"contained"} color={"primary"} component={Link} to="/">
+          <Typography className={classes.aboutText}>{apiMessage}</Typography>
+        </Grid>
+        <Grid container>
+          <Button
+            className={classes.button}
+            variant={"contained"}
+            color={"primary"}
+            component={Link}
+            to="/"
+          >
             {translator.get(EKey.backToHome)}
           </Button>
         </Grid>
